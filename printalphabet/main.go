2e17c23830c576967a42bedd7e0ea2355b3d652f
ezhovnuvaty <jeka.hammer2015@gmail.com>
2019-10-16 06:19:15 +0600

    ---

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,11 +1,10 @@
 package main
-	import "github.com/01-edu/z01"
 
-	func main() {
-	//	fmt.Println("abcdefghijklmnopqrstuvwxyz\n")
-	for i='a'; i<='z'; i++{
+import "github.com/01-edu/z01"
+
+func main() {
+	for i := 'a'; i <= 'z'; i++ {
 		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 }
-
