9601088a513ca86ff3a2f1f70f0ff6c6364716eb
ezhovnuvaty <jeka.hammer2015@gmail.com>
2019-10-17 22:27:45 +0600

    atoi1

---
+++ basicatoi2.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	result := 0
+	for _, value := range s {
+		count := 0
+		if value < '0' || value > '9'{
+			return 0
+		}
+		for i := '1'; i <= value; i++ {
+			count++
+		}
+		result = result*10 + count
+	}
+	return result
+}
