cef39d3f8ae466d1897a17e7358182af389e241d
ezhovnuvaty <jeka.hammer2015@gmail.com>
2019-10-17 16:07:54 +0600

    fix

--- printstr.go
+++ printstr.go
@@ -3,7 +3,8 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-
-	for _, r := range str {
-		z01.PrintRune(r)
+	for a, char := range str {
+		z01.PrintRune(char)
+		a++
 	}
+}
