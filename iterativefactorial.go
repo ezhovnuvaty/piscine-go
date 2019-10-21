3019e9c24a47c38b89a00607e70d81307ffcce2e
ezhovnuvaty <jeka.hammer2015@gmail.com>
2019-10-21 14:28:17 +0600

    fix01

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -5,7 +5,7 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 || nb >= 2147483647 {
 		return 0
 	}
-	for i := 0; i <= nb; i++ {
+	for i := 1; i <= nb; i++ {
 		result = result * i
 	}
 	return result
