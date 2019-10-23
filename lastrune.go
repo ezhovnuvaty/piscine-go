1e7dd02d0bb89f73ddd5610d1bc8bdea1cfb125a
ezhovnuvaty <jeka.hammer2015@gmail.com>
2019-10-23 18:42:20 +0600

    lstrn

---
+++ lastrune.go
@@ -0,0 +1,14 @@
+package piscine
+
+import 	"github.com/01-edu/z01"
+
+func LastRune(s string) rune {
+	result := []rune(s)
+
+	for index, value := range result {
+		if index == len(result)-1 {
+			return value
+		}
+	}
+	return 0
+}
